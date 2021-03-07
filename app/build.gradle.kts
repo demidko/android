97763521d4d326c164203ce1e99655a2a63ab967
Daniil Demidko <demidko@farpost.com>
2021-03-08 06:54:14 +1000

    Оптимизации

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+import com.android.build.gradle.internal.dsl.SigningConfig
+import org.gradle.api.JavaVersion.VERSION_1_8
+
+plugins {
+  id("com.android.application")
+  kotlin("android")
+}
+
+android {
+  compileSdkVersion(30)
+  buildToolsVersion("30.0.3")
+
+  defaultConfig {
+    applicationId = "gq.reactive.moneymap"
+    minSdkVersion(22)
+    targetSdkVersion(30)
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = true
+      isShrinkResources = true
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
+    }
+
+    register("optimized") {
+      isMinifyEnabled = true
+      isShrinkResources = true
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
+      signingConfig = SigningConfig("shared").apply {
+        keyAlias = "key0"
+        keyPassword = "123456"
+        storeFile = file("key")
+        storePassword = "123456"
+      }
+    }
+  }
+  compileOptions {
+    sourceCompatibility = VERSION_1_8
+    targetCompatibility = VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+    useIR = true
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.0.0-beta01"
+  }
+
+  packagingOptions {
+    resources {
+      excludes.add("META-INF/*.kotlin_module")
+      excludes.add("**.kotlin_builtins")
+      excludes.add("**.kotlin_metadata")
+    }
+  }
+}
+
+dependencies {
+
+  implementation("androidx.core:core-ktx:1.3.2")
+  implementation("androidx.appcompat:appcompat:1.2.0")
+  implementation("com.google.android.material:material:1.3.0")
+  implementation("androidx.compose.ui:ui:1.0.0-beta01")
+  implementation("androidx.compose.material:material:1.0.0-beta01")
+  implementation("androidx.compose.ui:ui-tooling:1.0.0-beta01")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0")
+  implementation("androidx.activity:activity-compose:1.3.0-alpha03")
+  testImplementation("junit:junit:4.13.2")
+  androidTestImplementation("androidx.test.ext:junit:1.1.2")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 ## Moneymap
 
-Financial app for the lazy by [concept](https://www.figma.com/file/FjvhOfJJbaFBtRYMcJVnHm/%D0%94%D0%B5%D0%BD%D1%8C%D0%B3%D0%BE%D0%BF%D0%BB%D0%B0%D0%BD?node-id=0%3A1)
+Financial app for the lazy
+by [concept](https://www.figma.com/file/FjvhOfJJbaFBtRYMcJVnHm/%D0%94%D0%B5%D0%BD%D1%8C%D0%B3%D0%BE%D0%BF%D0%BB%D0%B0%D0%BD?node-id=0%3A1)
 
-### Build apk
-* `assembleRelease`
+### Build
+
+1. Run command `./gradlew assembleOptimized` in the repo.
+1. Your app will be located at `./app/build/outputs/apk/optimized/*.apk`
